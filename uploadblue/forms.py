3e361cf39d1c587efc5f384142256bc684111e06
Mark Scrimshire <mark@96-24-68-166.sfo.clearwire-wmx.net>
2011-09-25 02:24:33 -0700

    image for green submit

--- uploadblue/forms.py
+++ uploadblue/forms.py
@@ -5,7 +5,7 @@ from django import forms
 
 
 LEVEL_CHOICES=(
-            ('0','Send All information (not reccomended)'),
+            ('0','Send All information (not recommended)'),
             ('1','Remove name and contact information'),
             ('2','Also remove family history and military service'),
             ('3','Also remove medications'),)
