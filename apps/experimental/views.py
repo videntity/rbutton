2f8d67ef0f693f50d4fea577036011f04f446e32
Mark Scrimshire <mscrimshire@gmail.com>
2012-03-11 18:27:14 -0400

    moved tes tot experimental folder

--- apps/experimental/views.py
+++ apps/experimental/views.py
@@ -1,1 +1,32 @@
-__author__ = 'mark'
+"""
+    Testing picup
+"""
+from apps.experimental.utils import is_iOS, is_iOS_browser, utils_available
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def browser_test(request):
+
+    received = request
+    bt = is_iOS(received)
+
+    print utils_available()
+
+    print "Testing for Safari Mobile Browser:"
+    print bt
+
+    ba = is_iOS_browser(received)
+
+    fu = ""
+
+    return render_to_response('experimental/evaluate.html',
+            {
+            'result':bt,
+            'found':ba,
+            'file_upload': fu,
+
+
+            },
+        RequestContext(request))
+
+
